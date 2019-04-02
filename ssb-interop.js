d8907852d45fc700c622eeaff3983202552b65e3
Christopher Kochan <ckochan@apple.com>
2019-04-02 16:32:01 -0700

    More fixing

--- ssb-interop.js
+++ ssb-interop.js
@@ -120,10 +120,8 @@ document.addEventListener("DOMContentLoaded", function() {
 }
 
 body {
-    /* src: url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');
-*/
+    /* src: url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');*/
     src: url('https://fonts.googleapis.com/css?family=Lato:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');
-*/
     text-rendering: optimizeLegibility important;
     word-spacing: -1px;
 }
